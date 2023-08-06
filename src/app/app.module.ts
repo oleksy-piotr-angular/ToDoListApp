7d82a948ec477e28f65bfd7363c80ad4444cf7f4
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2023-08-06 17:47:00 +0200

    add First ngrx-effects file and change ngrx version to more appropriate for this angular version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ToastrModule } from 'ngx-toastr';
@@ -7,12 +7,12 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './shared/components/header/header.component';
-import { appReducer } from './store/app.state';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppMaterialModule } from './AngularMaterial/material-module';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './components/home/home.component';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, HomeComponent],
@@ -25,6 +25,7 @@ import { HomeComponent } from './components/home/home.component';
       tapToDismiss: true,
       disableTimeOut: true,
     }),
+    EffectsModule.forFeature([]),
     StoreModule.forRoot({}),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
@@ -36,6 +37,9 @@ import { HomeComponent } from './components/home/home.component';
       },
     }),
     NgbModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    EffectsModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent],
